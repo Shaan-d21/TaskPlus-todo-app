b8bec1c254ed9fcd2ce002ff120c6941e9327dc4
shaan <shaan.dewang212@gmail.com>
2025-06-07 19:07:34 +0530

    Removed Home From Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () => {
         <div className="logo">
             <span className='font-bold text-xl mx-8'>Task Plus <span className='text-2xl'>+</span></span>
         </div>
-      <ul className="flex gap-8 mx-9">
+      {/* <ul className="flex gap-8 mx-9">
         <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
         <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
-      </ul>
+      </ul> */}
     </nav>
   )
 }
